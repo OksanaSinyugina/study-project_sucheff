28ef91ea7adf67d487f3c9d3dfedcff03fd144db
OksanaSinyugina <111419697+OksanaSinyugina@users.noreply.github.com>
2022-09-14 19:12:03 +0300

    created new blocks
    slider, favorites, seo text, footer, copyright

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        //сюда пишем свойства
+        arrows: false,
+        dots: true,
+        fade: true,
+        cssEase: 'linear',
+        autoplay: true,
+    });
+});
